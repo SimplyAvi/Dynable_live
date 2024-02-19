81059d65f53ced1abd35cc39fb7a37175734c15c
mjc695 <jenningtan21@gmail.com>
2024-02-19 13:16:48 -0500

    created routing with react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import logo from './logo.svg';
+import { Routes, Route} from 'react-router-dom'
+
+// import Homepage from './pages/Homepage';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Dynable is now Eatems
         </p>
+        <div>
+          <Routes>
+            {/* <Route exact path='/' Component={Homepage} /> */}
+          </Routes>
+        </div>
       </header>
     </div>
   );
