39787f5506ffb598921570ca49011b3d68d73593
mjc695 <jenningtan21@gmail.com>
2024-02-22 18:49:18 -0500

    display search results using a list of cards

--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -4,23 +4,20 @@ import axios from 'axios'
 import FormInput from './FormInput'
 import './Searchbar.css'
 
-const Searchbar = () => {
+const Searchbar = ({setSearchResults}) => {
 
     const [searchVal, setSearchVal] = useState('')
-    const [imgUrl, setImgUrl] = useState('')
 
     const handleTextChange = (input) =>{
-        console.log(input.target)
         setSearchVal(input.target.value)
     }
     
-    const handleSubmit = async () =>{
+    const handleSubmit = async (event) =>{
+        event.preventDefault()
         console.log('submitting:', searchVal)
         try {
             const response = await axios.get(`https://api.edamam.com/api/food-database/v2/parser?app_id=3b4e6a49&app_key=8d49f61369d7dda4935235b21c07a612&ingr=${searchVal}&nutrition-type=cooking`);
-            console.log(response);
-            console.log(response.data.hints[0].food)
-            setImgUrl(response.data.hints[0].food.image)
+            setSearchResults(response.data)
         } catch (error) {
             console.error(error);
         }
@@ -38,7 +35,6 @@ const Searchbar = () => {
                     handleChange={handleTextChange}/>
                 <button className='custom-button' type='button' onClick={handleSubmit}>SUBMIT</button>
             </form>
-            <img src={imgUrl}/>
         </div>
     )
 }
