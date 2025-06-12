c3799a264df8113f10bb5362547d228017089a1f
JusJJ <justinll2235@gmail.com>
2025-06-12 03:52:15 -0400

    feat: add logout functionality to header with styled button

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -9,18 +9,29 @@
  * - Login/Signup buttons for guests
  * - Profile access for authenticated users
  * - Dynable logo with home navigation
+ * - Logout functionality for authenticated users
  */
 
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
-import { useSelector } from 'react-redux'
-import { selectIsAuthenticated, selectCurrentUser } from '../../redux/authSlice'
+import { useSelector, useDispatch } from 'react-redux'
+import { selectIsAuthenticated, selectCurrentUser, logout } from '../../redux/authSlice'
 import './Header.css'
 
 const Header = () => {
     const navigate = useNavigate()
+    const dispatch = useDispatch()
     const isAuthenticated = useSelector((state) => selectIsAuthenticated(state))
     const user = useSelector((state) => selectCurrentUser(state))
+
+    const handleLogout = () => {
+        // Clear local storage
+        localStorage.removeItem('token')
+        // Dispatch logout action
+        dispatch(logout())
+        // Navigate to home page
+        navigate('/')
+    }
 
     return (
         <header className="header">
@@ -35,6 +46,9 @@ const Header = () => {
                         <span className="welcome-text">Welcome, {user?.name || user?.email}</span>
                         <button className="nav-button" onClick={() => navigate('/profile')}>
                             Profile
+                        </button>
+                        <button className="nav-button logout" onClick={handleLogout}>
+                            Logout
                         </button>
                     </>
                 ) : (
